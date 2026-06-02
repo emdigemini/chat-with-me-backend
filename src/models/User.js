fe3a9aff52fb670e78b5f79e3b710f4b246bd44f
emdigemini <emdigemini@gmail.com>
2026-06-02 21:59:28 +0800

    fix user model

--- src/models/User.js
+++ src/models/User.js
@@ -21,10 +21,6 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     trim: true,
     match: [/^\S+@gmail\.com$/, 'Please enter a valid gmail address'],
-  },
-  chatIDs: {
-    type: [String],
-    default: []
   }
 }, { timestamps: true });
 
