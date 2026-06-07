5fe4370be7103cd3f468b6130a5fa71c230eeeaf
emdigemini <emdigemini@gmail.com>
2026-06-07 21:43:58 +0800

    add check auth/token for mobile and web

---
+++ src/middleware/authentication.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+const authentication = async (req: Request, res: Response, next: NextFunction) => {
+  let token = req.cookies?.token || req.headers["authorization"]?.split(" ")[1];
+
+  if(!token)
+    return res.status(401).json({ message: "No token, access denied." });
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({message: "Invalid token"});
+  }
+}
+
+export default authentication;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,5 +7,6 @@ router.post('/verify-email', verifyEmail);
 router.post('/confirm-verification', confirmEmailVerification);
 router.post('/create-account', createAccount);
 router.post('/login-account', loginAccount);
+router.get('/check-auth', )
 
 export default router
