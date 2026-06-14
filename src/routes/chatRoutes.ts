a4b2ccd66febeb905a56e1e497a45328c07c1c70
emdigemini <emdigemini@gmail.com>
2026-06-14 09:47:42 +0800

    addd routes for getNewMessages

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { addNewChat, getChats } from "../controller/chatController";
+import { addNewChat, getChats, getNewMessages } from "../controller/chatController";
 
 const router = express.Router();
 
 router.post("/new-chat", addNewChat);
 router.get("/chat-list/:id", getChats);
+router.get("/new-messages", getNewMessages);
 
 export default router;
